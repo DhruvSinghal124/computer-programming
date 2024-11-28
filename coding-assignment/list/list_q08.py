667ef2ce163f628b81164525331a4c1a157cee35
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:59:32 +0530

    Update list_q08.py

--- coding-assignment/list/list_q08.py
+++ coding-assignment/list/list_q08.py
@@ -1,1 +1,8 @@
+# Problem 8: Min and max elements in a list
+def find_min_max(lst):
+    return min(lst), max(lst)
+
+# Test the function
+numbers = [10, 20, 4, 45, 99]
+print(find_min_max(numbers))
 
