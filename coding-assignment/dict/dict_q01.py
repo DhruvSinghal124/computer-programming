6f54d81173e969132b9ab366bd2c8c0f8c593cbc
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:39:55 +0530

    Update dict_q01.py

--- coding-assignment/dict/dict_q01.py
+++ coding-assignment/dict/dict_q01.py
@@ -1,1 +1,5 @@
+# Problem 1: Create dictionary from list of tuples
+tuples = [('name', 'Alice'), ('age', 30), ('city', 'New York')]
+dictionary = dict(tuples)
+print("Dictionary from list of tuples:", dictionary)
 
