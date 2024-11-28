6cb59bac9444649ab454730ab32874847497d167
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 08:55:01 +0530

    Update string_q03.py

--- coding-assignment/string/string_q03.py
+++ coding-assignment/string/string_q03.py
@@ -1,1 +1,7 @@
+Problem 1: Reverse a String
+Description: Write a program to reverse a given string.
 
+def reverse_string(input_string):
+    return input_string[::-1]
+
+print(reverse_string("hello")) 
