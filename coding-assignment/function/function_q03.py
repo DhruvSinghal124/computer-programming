e54838264e20d432f40acfcc3f435b97ee8b8dc2
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:49:57 +0530

    Update function_q03.py

--- coding-assignment/function/function_q03.py
+++ coding-assignment/function/function_q03.py
@@ -1,1 +1,8 @@
+# Problem 3: Find unique elements in a list
+def unique_elements(lst):
+    return list(set(lst))
+
+# Test the function
+lst = [1, 2, 2, 3, 4, 4, 5]
+print("Unique elements:", unique_elements(lst))
 
