666d2a23418a05752b85ce55aa06444e1eef3626
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:19:59 +0530

    Update string_q10.py

--- coding-assignment/string/string_q10.py
+++ coding-assignment/string/string_q10.py
@@ -1,1 +1,8 @@
+# Problem 10: Replace spaces with hyphens
+def replace_spaces(string):
+    return string.replace(" ", "-")
+
+# Example usage
+string = "hello world"
+print(f"String after replacing spaces: '{replace_spaces(string)}'.")
 
