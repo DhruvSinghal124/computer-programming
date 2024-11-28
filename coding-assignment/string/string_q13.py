e103699878663a38e399a6ff488a99fa2d40f4fa
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:24:28 +0530

    Update string_q13.py

--- coding-assignment/string/string_q13.py
+++ coding-assignment/string/string_q13.py
@@ -1,1 +1,8 @@
 
+# Problem 13: Convert a string to lowercase
+def to_lowercase(string):
+    return string.lower()
+
+# Example usage
+string = "Hello World"
+print(f"Lowercase version of '{string}' is '{to_lowercase(string)}'.")
