d1e8615564a13b0ec3eaa4faad282a9248552b26
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:52:29 +0530

    Update function_q09.py

--- coding-assignment/function/function_q09.py
+++ coding-assignment/function/function_q09.py
@@ -1,1 +1,8 @@
+# Problem 9: Longest word in a sentence
+def longest_word(sentence):
+    words = sentence.split()
+    return max(words, key=len)
+
+# Test the function
+print(longest_word("I love programming in Python"))
 
