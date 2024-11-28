58761972beba5e1a763fa75e5c0d527e35246d4d
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:57:55 +0530

    Update list_q01.py

--- coding-assignment/list/list_q01.py
+++ coding-assignment/list/list_q01.py
@@ -1,1 +1,9 @@
+# Problem 1: Merge two lists
+def merge_lists(list1, list2):
+    return list1 + list2
+
+# Test the function
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+print(merge_lists(list1, list2))
 
