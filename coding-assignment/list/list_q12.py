c6e7d97ac8fe7a56592fe87a46da1072426d2bff
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 15:00:31 +0530

    Update list_q12.py

--- coding-assignment/list/list_q12.py
+++ coding-assignment/list/list_q12.py
@@ -1,1 +1,11 @@
+# Problem 12: Product of all elements in a list
+def product_of_elements(lst):
+    product = 1
+    for num in lst:
+        product *= num
+    return product
+
+# Test the function
+numbers = [1, 2, 3, 4]
+print(product_of_elements(numbers))
 
