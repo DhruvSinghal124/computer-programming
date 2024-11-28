4f33dd029d8ab56cc52b8a5f5320b7a03ff7b6ae
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:58:08 +0530

    Update list_q02.py

--- coding-assignment/list/list_q02.py
+++ coding-assignment/list/list_q02.py
@@ -1,1 +1,9 @@
+# Problem 2: Find the second largest element
+def second_largest(lst):
+    lst.sort()
+    return lst[-2]
+
+# Test the function
+numbers = [10, 20, 4, 45, 99]
+print(second_largest(numbers))
 
