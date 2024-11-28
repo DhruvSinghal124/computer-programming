2f13c375fb1551d531e5e6f52e260dff8dc06bb9
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:59:05 +0530

    Update list_q06.py

--- coding-assignment/list/list_q06.py
+++ coding-assignment/list/list_q06.py
@@ -1,1 +1,9 @@
+# Problem 6: Intersection of two lists
+def intersection(list1, list2):
+    return [item for item in list1 if item in list2]
+
+# Test the function
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+print(intersection(list1, list2))
 
