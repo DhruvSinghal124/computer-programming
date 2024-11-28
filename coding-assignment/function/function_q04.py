eba19e5f98e368e10f160708deea00cf26bc08c3
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:50:12 +0530

    Update function_q04.py

--- coding-assignment/function/function_q04.py
+++ coding-assignment/function/function_q04.py
@@ -1,1 +1,9 @@
+# Problem 4: Find common elements between two lists
+def common_elements(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+# Test the function
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7]
+print("Common elements:", common_elements(list1, list2))
 
