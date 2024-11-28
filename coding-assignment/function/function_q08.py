d0193480a797474786aec7674f43d800cbe613d0
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:51:52 +0530

    Update function_q08.py

--- coding-assignment/function/function_q08.py
+++ coding-assignment/function/function_q08.py
@@ -1,1 +1,8 @@
+# Problem 8: Check if a number is perfect
+def is_perfect(num):
+    divisors_sum = sum(i for i in range(1, num) if num % i == 0)
+    return divisors_sum == num
+
+# Test the function
+print(is_perfect(28))
 
