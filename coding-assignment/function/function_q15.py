37306865111c1c1960d146feea23e5f7210aa582
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:54:11 +0530

    Update function_q15.py

--- coding-assignment/function/function_q15.py
+++ coding-assignment/function/function_q15.py
@@ -1,1 +1,11 @@
+# Problem 15: Prime numbers in a range
+def primes_in_range(start, end):
+    primes = []
+    for num in range(start, end+1):
+        if all(num % i != 0 for i in range(2, int(num**0.5) + 1)):
+            primes.append(num)
+    return primes
+
+# Test the function
+print(primes_in_range(10, 20))
 
