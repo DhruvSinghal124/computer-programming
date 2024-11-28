8e971cc9ddaca850522111cd9dd46965a32fbc81
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:42:50 +0530

    Update dict_q06.py

--- coding-assignment/dict/dict_q06.py
+++ coding-assignment/dict/dict_q06.py
@@ -1,1 +1,7 @@
+# Problem 6: Check if a key exists
+person = {'name': 'Alice', 'age': 25, 'city': 'New York'}
+if 'age' in person:
+    print("Age exists in the dictionary")
+else:
+    print("Age does not exist")
 
