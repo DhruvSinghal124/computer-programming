5c231cbdebcc84357ae08476869348b0688c7039
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:45:03 +0530

    Update dict_q09.py

--- coding-assignment/dict/dict_q09.py
+++ coding-assignment/dict/dict_q09.py
@@ -1,1 +1,5 @@
+# Problem 9: Loop through dictionary items (key-value pairs)
+person = {'name': 'Alice', 'age': 25, 'city': 'New York'}
+for key, value in person.items():
+    print(key, ":", value)
 
