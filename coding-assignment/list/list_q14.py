033fe5352d8f2abc92d131a18180846293b61a42
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 15:00:57 +0530

    Update list_q14.py

--- coding-assignment/list/list_q14.py
+++ coding-assignment/list/list_q14.py
@@ -1,1 +1,8 @@
+# Problem 14: Flatten a list of lists
+def flatten_list(lst):
+    return [item for sublist in lst for item in sublist]
+
+# Test the function
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(flatten_list(list_of_lists))
 
