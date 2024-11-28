b3e535f989a960b4f1a067ff5c127642e6f2d021
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:50:43 +0530

    Update function_q05.py

--- coding-assignment/function/function_q05.py
+++ coding-assignment/function/function_q05.py
@@ -1,1 +1,9 @@
+# Problem 5: Factorial of a number using recursion
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+# Test the function
+print(factorial(5))
 
