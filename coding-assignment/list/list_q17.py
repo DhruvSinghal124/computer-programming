d52cc0ec2ddbf98a0d3e6feab41584f0ee94f121
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 15:01:36 +0530

    Update list_q17.py

--- coding-assignment/list/list_q17.py
+++ coding-assignment/list/list_q17.py
@@ -1,1 +1,8 @@
+# Problem 17: Rotate a list by n positions
+def rotate_list(lst, n):
+    return lst[n:] + lst[:n]
+
+# Test the function
+numbers = [1, 2, 3, 4, 5]
+print(rotate_list(numbers, 2))
 
