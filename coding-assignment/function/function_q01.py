d255f3e2015cc8da1b7b79c911a1a7898a493b35
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:48:46 +0530

    Update function_q01.py

--- coding-assignment/function/function_q01.py
+++ coding-assignment/function/function_q01.py
@@ -1,1 +1,11 @@
+# Problem 1: Sum of all numbers in a list
+def sum_of_list(lst):
+    total = 0
+    for num in lst:
+        total += num
+    return total
+
+# Test the function
+numbers = [1, 2, 3, 4, 5]
+print("Sum of the list:", sum_of_list(numbers))
 
