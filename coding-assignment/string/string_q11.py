93c4de1e9cff4d6c427ebcd3b4509c85824dffe4
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:21:07 +0530

    Update string_q11.py

--- coding-assignment/string/string_q11.py
+++ coding-assignment/string/string_q11.py
@@ -1,1 +1,8 @@
+# Problem 11: Check if a string contains only digits
+def is_digits(string):
+    return string.isdigit()
+
+# Example usage
+string = "12345"
+print(f"'{string}' contains only digits: {is_digits(string)}.")
 
