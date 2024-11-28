16aade473d999790f351ab6ea204046cfbcba53b
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 15:02:11 +0530

    Update list_q19.py

--- coding-assignment/list/list_q19.py
+++ coding-assignment/list/list_q19.py
@@ -1,1 +1,9 @@
+# Problem 19: Add an element at the end
+def append_to_list(lst, element):
+    lst.append(element)
+    return lst
+
+# Test the function
+numbers = [1, 2, 3]
+print(append_to_list(numbers, 4))
 
