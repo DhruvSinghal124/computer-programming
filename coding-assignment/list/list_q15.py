99de255d3bc39bebd21d18c3bb652c978c6a7aee
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 15:01:09 +0530

    Update list_q15.py

--- coding-assignment/list/list_q15.py
+++ coding-assignment/list/list_q15.py
@@ -1,1 +1,11 @@
+# Problem 15: Most frequent element
+from collections import Counter
+
+def most_frequent(lst):
+    count = Counter(lst)
+    return count.most_common(1)[0][0]
+
+# Test the function
+numbers = [1, 2, 3, 3, 3, 4, 5]
+print(most_frequent(numbers))
 
