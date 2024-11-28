aac3c22377518b5af5a09bfa0837bfcf800151a3
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 15:02:24 +0530

    Update list_q20.py

--- coding-assignment/list/list_q20.py
+++ coding-assignment/list/list_q20.py
@@ -1,1 +1,10 @@
+# Problem 20: Remove element at specific index
+def remove_at_index(lst, index):
+    if 0 <= index < len(lst):
+        return lst[:index] + lst[index + 1:]
+    return lst
+
+# Test the function
+numbers = [1, 2, 3, 4, 5]
+print(remove_at_index(numbers, 2))
 
