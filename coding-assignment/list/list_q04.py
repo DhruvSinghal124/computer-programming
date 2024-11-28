f80ecbfaa917be0390ca0d95123d786480a38064
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:58:33 +0530

    Update list_q04.py

--- coding-assignment/list/list_q04.py
+++ coding-assignment/list/list_q04.py
@@ -1,1 +1,8 @@
+# Problem 4: Remove all occurrences of an element
+def remove_occurrences(lst, value):
+    return [item for item in lst if item != value]
+
+# Test the function
+numbers = [1, 2, 3, 2, 4, 2, 5]
+print(remove_occurrences(numbers, 2))
 
