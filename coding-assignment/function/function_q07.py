823e56ee8d6ab09fb909d73e691d11f0142f4416
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:51:30 +0530

    Update function_q07.py

--- coding-assignment/function/function_q07.py
+++ coding-assignment/function/function_q07.py
@@ -1,1 +1,12 @@
+# Problem 7: Fibonacci sequence
+def fibonacci(n):
+    a, b = 0, 1
+    fib_sequence = []
+    for _ in range(n):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    return fib_sequence
+
+# Test the function
+print(fibonacci(7))
 
