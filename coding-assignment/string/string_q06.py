b7e18c2f9e6213831a4ab487a511eef21d7c2a44
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 08:59:36 +0530

    Update string_q06.py

--- coding-assignment/string/string_q06.py
+++ coding-assignment/string/string_q06.py
@@ -1,1 +1,10 @@
+Problem 6: Find First and Last Characters of a String
+Description: Write a program that returns the first and last characters of the given string.
 
+def first_and_last_char(input_string):
+    if len(input_string) < 2:
+        return ""
+    return input_string[0] + input_string[-1]
+
+# Example usage
+print(first_and_last_char("hello"))
