f21ec0745b3e6c93e10053e7f013a5d29b80443c
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:52:48 +0530

    Update function_q10.py

--- coding-assignment/function/function_q10.py
+++ coding-assignment/function/function_q10.py
@@ -1,1 +1,8 @@
+# Problem 10: Count vowels in a string
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    return sum(1 for char in s if char in vowels)
+
+# Test the function
+print(count_vowels("Hello World"))
 
