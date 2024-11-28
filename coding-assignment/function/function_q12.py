1c29f9dc3c0e048e96cf0b44846b8fd93325ab72
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:53:20 +0530

    Update function_q12.py

--- coding-assignment/function/function_q12.py
+++ coding-assignment/function/function_q12.py
@@ -1,1 +1,7 @@
+# Problem 12: Remove all occurrences of an element from a list
+def remove_element(lst, element):
+    return [x for x in lst if x != element]
+
+# Test the function
+print(remove_element([1, 2, 3, 4, 2, 5], 2))
 
