45e4b635b5144976215bca43815f07f57b60f68d
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:59:44 +0530

    Update list_q09.py

--- coding-assignment/list/list_q09.py
+++ coding-assignment/list/list_q09.py
@@ -1,1 +1,8 @@
+# Problem 9: Remove duplicates from a list
+def remove_duplicates(lst):
+    return list(set(lst))
+
+# Test the function
+numbers = [1, 2, 2, 3, 4, 4, 5]
+print(remove_duplicates(numbers))
 
