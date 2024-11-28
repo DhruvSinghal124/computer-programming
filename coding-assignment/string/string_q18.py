09685b45ccb466001f0881d8257cb0c363f8fff5
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:28:23 +0530

    Update string_q18.py

--- coding-assignment/string/string_q18.py
+++ coding-assignment/string/string_q18.py
@@ -1,1 +1,8 @@
+# Problem 18: Convert a string to a list of words
+def string_to_words(string):
+    return string.split()
+
+# Example usage
+string = "Python is fun"
+print(f"Words in the string: {string_to_words(string)}.")
 
