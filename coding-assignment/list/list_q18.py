c7188bbfda766009426723d8eeae0b905cab533c
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 15:01:52 +0530

    Update list_q18.py

--- coding-assignment/list/list_q18.py
+++ coding-assignment/list/list_q18.py
@@ -1,1 +1,9 @@
+# Problem 18: Difference between two lists
+def list_difference(lst1, lst2):
+    return [item for item in lst1 if item not in lst2]
+
+# Test the function
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+print(list_difference(list
 
