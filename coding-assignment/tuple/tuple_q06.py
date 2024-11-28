61af7a7cadd2a5840c4313bcafa14bb8c7b9fc68
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:34:36 +0530

    Update tuple_q06.py

--- coding-assignment/tuple/tuple_q06.py
+++ coding-assignment/tuple/tuple_q06.py
@@ -1,1 +1,6 @@
+# Problem 6: Access nested tuple elements
+nested_tuple = (10, 20, (30, 40, (50, 60)))
+print("Nested Tuple:", nested_tuple)
+print("Access 40:", nested_tuple[2][1])
+print("Access 50:", nested_tuple[2][2][0])
 
