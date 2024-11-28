660d2878cd6014c92d280ca280fec8cc71d7c446
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 15:00:02 +0530

    Update list_q10.py

--- coding-assignment/list/list_q10.py
+++ coding-assignment/list/list_q10.py
@@ -1,1 +1,8 @@
+# Problem 10: Find index of an element
+def find_index(lst, element):
+    return lst.index(element)
+
+# Test the function
+numbers = [10, 20, 30, 40, 50]
+print(find_index(numbers, 30))
 
