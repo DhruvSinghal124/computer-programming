b23ec09e1c567f37d14abb213dc399978e23e8e9
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:36:26 +0530

    Update tuple_q10.py

--- coding-assignment/tuple/tuple_q10.py
+++ coding-assignment/tuple/tuple_q10.py
@@ -1,1 +1,6 @@
+# Problem 10: Convert a list of tuples to dictionary
+tuples = [("name", "John"), ("age", 30), ("city", "New York")]
+dictionary = dict(tuples)
+print("List of Tuples:", tuples)
+print("Converted Dictionary:", dictionary)
 
