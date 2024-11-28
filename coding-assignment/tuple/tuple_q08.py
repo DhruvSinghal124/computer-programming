42a1d7f545c9e525ef28c2c67cd945e806de034e
Dhruv Singhal <dhruvsinghalfts124@gmail.com>
2024-11-28 14:35:25 +0530

    Update tuple_q08.py

--- coding-assignment/tuple/tuple_q08.py
+++ coding-assignment/tuple/tuple_q08.py
@@ -1,1 +1,8 @@
+# Problem 8: Find common elements in tuples
+t1 = (1, 2, 3, 4)
+t2 = (3, 4, 5, 6)
+intersection = tuple(set(t1) & set(t2))
+print("Tuple 1:", t1)
+print("Tuple 2:", t2)
+print("Common Elements:", intersection)
 
